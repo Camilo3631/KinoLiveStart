3c98191d709fca0ee4bd161f8d5baf31a7fcf3d1
Camilo Acosta <camiloacosta@MacBook-Air-de-Camilo.local>
2025-05-29 00:57:46 -0500

    Clase 19: Despligue Seguro de Aplicaciónes Web y Proteción de API Keys

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
-  // Creamos una instancia de axios
-  const api = axios.create({
-    // Base URL
-    baseURL: 'https://api.themoviedb.org/3/',
-    // api_key como parámetro
-    params: {
-      'api_key': '030eada77e494e280d243a5356401f1a',
-    },
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8'
-    },
-  })
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    'api_key': '030eada77e494e280d243a5356401f1a',
+  },
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+});
+
+export default api;
--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-
+let onScroll;
 
 
 searchButton.addEventListener('click', () => {
